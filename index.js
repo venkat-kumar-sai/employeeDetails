a583f59eada5b84d4bdfe9adb79ad7313b2864cf
Venkat Sai Uttej Kumar <vsukuma1@in.ibm.com>
2020-01-09 17:00:41 +0530

    ss

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.get('/error',function(req,res){
-    res.status(500).send("error")
+    res.status(500).send(err)
 })
 app.get('/',function(req,res){
     res.send("hai")
