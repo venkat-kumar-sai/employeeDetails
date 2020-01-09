6e98cd0b48680c58f04ff3246e9c1035539bd821
Venkat Sai Uttej Kumar <vsukuma1@in.ibm.com>
2020-01-09 16:45:38 +0530

    api created

--- index.js
+++ index.js
@@ -8,82 +8,82 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.get('/',function(req,res){
     res.send("hai")
 })
-// var mysqlConnection = mysql.createConnection({
-//     host:'localhost',
-//     user:'root',
-//     password:'',
-//     database:'employeedb'
-// });
-// mysqlConnection.connect((err)=>{
-//     if(!err)
-//     console.log('database connected');
-//     else
-//     console.log('error')
-// });
-// //gets all employee details
-// app.get('/getEmployeeDetails',(req,res)=>{
-//     sql = 'select * from employee';
-//     mysqlConnection.query(sql,(err,rows,field)=>{
-//         if(!err)
-//         console.log(rows)
+var mysqlConnection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'employeedb'
+});
+mysqlConnection.connect((err)=>{
+    if(!err)
+    console.log('database connected');
+    else
+    console.log('error')
+});
+//gets all employee details
+app.get('/getEmployeeDetails',(req,res)=>{
+    sql = 'select * from employee';
+    mysqlConnection.query(sql,(err,rows,field)=>{
+        if(!err)
+        console.log(rows)
      
-//         else
-//         console.log('error')
-//         res.send(rows);
-//     })
-// });
-// //gets an employee details by using id
-// app.get('/getEmployeeIdDetails/:id',(req,res)=>{
-//     mysqlConnection.query('select * from employee where empId = ?',[req.params.id],(err,rows,field)=>{
-//         if(!err)
-//         console.log(rows)
-//         else
-//         console.log('error')
-//         res.send(rows)
-//     })
-// });
-// //delete an employee
-// app.delete('/deleteEmployeeDetails/:id',(req,res)=>{
-//     mysqlConnection.query('delete from employee where empId = ?',[req.params.id],(err,rows,field)=>{
-//         if(!err)
-//         console.log("deleted successfully")
-//         else
-//         console.log('error')
-//         res.send("deleted sucessfully")
-//     })
-// });
-// //insert an employee 
-// app.post('/insertEmployeeDetails', function(req, res) {
-//        console.log(req.body);
-//        var empId = req.body.empId;
-//        var name = req.body.name;
-//        var empCode = req.body.empCode;
-//        var salary = req.body.salary;
-//       // if(req.body.name){console.log(req.body.name)}else{console.log("correct")}
+        else
+        console.log('error')
+        res.send(rows);
+    })
+});
+//gets an employee details by using id
+app.get('/getEmployeeIdDetails/:id',(req,res)=>{
+    mysqlConnection.query('select * from employee where empId = ?',[req.params.id],(err,rows,field)=>{
+        if(!err)
+        console.log(rows)
+        else
+        console.log('error')
+        res.send(rows)
+    })
+});
+//delete an employee
+app.delete('/deleteEmployeeDetails/:id',(req,res)=>{
+    mysqlConnection.query('delete from employee where empId = ?',[req.params.id],(err,rows,field)=>{
+        if(!err)
+        console.log("deleted successfully")
+        else
+        console.log('error')
+        res.send("deleted sucessfully")
+    })
+});
+//insert an employee 
+app.post('/insertEmployeeDetails', function(req, res) {
+       console.log(req.body);
+       var empId = req.body.empId;
+       var name = req.body.name;
+       var empCode = req.body.empCode;
+       var salary = req.body.salary;
+      // if(req.body.name){console.log(req.body.name)}else{console.log("correct")}
      
-//     mysqlConnection.query(`Insert into employee(name,empCode,salary) VALUES ("${name}","${empCode}",${salary})`,function(err,rows, result)      
-//     {                                                      
-//       if (err)
-//          throw err;
+    mysqlConnection.query(`Insert into employee(name,empCode,salary) VALUES ("${name}","${empCode}",${salary})`,function(err,rows, result)      
+    {                                                      
+      if (err)
+         throw err;
 
-//          console.log("1 record inserted");
-//          res.send("1 record inserted sucessfully");
-//     }
+         console.log("1 record inserted");
+         res.send("1 record inserted sucessfully");
+    }
 
-//     )
+    )
    
-// });
+});
 
-// //update employee details
-//     app.put('/updateEmployeeDetails', function(req, res) {
+//update employee details
+    app.put('/updateEmployeeDetails', function(req, res) {
            
-//          console.log("hello");
-//          mysqlConnection.query(`update employee set name = "${req.body.name}" where salary = "${req.body.salary}"`,function(err,result){
-//              if (err)
-//              throw err;
-//          })
-//          console.log('updated');
-//          res.send("1 record updated sucessfully")
-//          })
+         console.log("hello");
+         mysqlConnection.query(`update employee set name = "${req.body.name}" where salary = "${req.body.salary}"`,function(err,result){
+             if (err)
+             throw err;
+         })
+         console.log('updated');
+         res.send("1 record updated sucessfully")
+         })
 var port = process.env.PORT || 4000;
 app.listen(port,(req,res)=>{console.log("port 4000")});
